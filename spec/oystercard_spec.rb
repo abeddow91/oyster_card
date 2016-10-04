4c191d188ec9c0b13d6d4ee2fe6abc65a70cb3b3
Anna Beddow <anna.beddow@gmail.com>
2016-10-04 17:38:15 +0100

    refactored test with context and nested context blocks

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,77 +11,64 @@ describe OysterCard do
     @deduct_value = 1
   end
 
-  it 'adds top-up to balance' do
-    card.top_up(0)
-    expect(card.balance).to eq 0
-  end
-
-  it 'raises an error if balance is more than 90' do
-    card.top_up(@max_top_up)
-    expect {card.top_up(@top_up_value)}.to raise_error "balance cannot exceed #{OysterCard::MAX_LIMIT} pounds"
-  end
-
   it 'returns false for in_journey' do
   expect(card.in_journey?).to eq false
-  end
-
-  it 'touch_in to change in_journey to true' do
-    card.top_up(@top_up_value)
-    expect{card.touch_in(station)}.to change{card.in_journey?}.from(false).to(true)
-  end
-
-  it 'touch_out to change in_journey to false' do
-    card.top_up(@top_up_value)
-    card.touch_in(station)
-    expect{card.touch_out(station)}.to change{card.in_journey?}.from(true).to(false)
   end
 
   it 'touch_in raises an error if minimum fare not met' do
     expect{card.touch_in(station)}.to raise_error "insufficient funds to complete journey"
   end
 
-  it 'deducts minimum fare from balance' do
-    card.top_up(@top_up_value)
-    expect{card.touch_out(station)}.to change{card.balance}.by( 0 - OysterCard::MINIMUM_FARE)
+  context 'card is topped up' do
+    before do
+      card.top_up(@top_up_value)
+    end
+
+    it 'adds top-up to balance' do
+      expect(card.balance).to eq @top_up_value
+    end
+
+    it 'deducts minimum fare from balance' do
+      expect{card.touch_out(station)}.to change{card.balance}.by( 0 - OysterCard::MINIMUM_FARE)
+    end
+
+    it 'raises an error if balance is more than 90' do
+      expect {card.top_up(@max_top_up)}.to raise_error "balance cannot exceed #{OysterCard::MAX_LIMIT} pounds"
+    end
+
+    it 'touch_in to change in_journey to true' do
+      expect{card.touch_in(station)}.to change{card.in_journey?}.from(false).to(true)
+    end
+
+    it "sets the exit station" do
+      card.touch_out(station)
+      expect(card.exit_station).to eq station
+    end
+
+    it 'changes the hash key to entry station' do
+      expect{card.touch_in(station)}.to change{card.journey_history}.from({}).to({station => nil})
+    end
+
+    context 'card has been topped up and touched in' do
+      before do
+        card.touch_in(station)
+      end
+      it 'touch_out to change in_journey to false' do
+        expect{card.touch_out(station)}.to change{card.in_journey?}.from(true).to(false)
+      end
+
+      it "sets the entry station" do
+        expect(card.entry_station).to eq station
+      end
+
+      it 'sets the entry station to nil' do
+        expect{card.touch_out(station)}.to change{card.entry_station}.from(station).to(nil)
+      end
+
+      it 'changes the hash value to exit station' do
+        expect{card.touch_out(station)}.to change{card.journey_history}.from({station => nil}).to({station => station})
+      end
+    end
   end
-
-  it "sets the entry station" do
-    card.top_up(@top_up_value)
-    card.touch_in(station)
-    expect(card.entry_station).to eq station
-  end
-
-  it 'sets the entry station to nil' do
-    card.top_up(@top_up_value)
-    card.touch_in(station)
-    expect{card.touch_out(station)}.to change{card.entry_station}.from(station).to(nil)
-  end
-
-  it 'responds to journey history' do
-    expect(card).to respond_to(:journey_history)
-  end
-
-  it "sets the exit station" do
-    card.top_up(@top_up_value)
-    card.touch_out(station)
-    expect(card.exit_station).to eq station
-  end
-
-  it 'responds to current_journey' do
-  expect(card).to respond_to(:current_journey)
-  end
-
-  it 'changes the hash key to entry station' do
-    card.top_up(@top_up_value)
-    expect{card.touch_in(station)}.to change{card.journey_history}.from({}).to({station => nil})
-  end
-
-  it 'changes the hash value to exit station' do
-    card.top_up(@top_up_value)
-    card.touch_in(station)
-    expect{card.touch_out(station)}.to change{card.journey_history}.from({station => nil}).to({station => station})
-  end
-
-
 
 end
